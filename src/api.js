e6e4a98ee6d4f6a064529c087c9a31cf35ea43b6
infosakshamsevasansthan-web <infosakshamsevasansthan@gmail.com>
2026-07-31 17:31:55 +0530

    Fix API base URL and improve console logs
    Updated the base URL for the API and changed console log formatting.

--- src/api.js
+++ src/api.js
@@ -1,13 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
-console.log("APP START");
-console.log(API.defaults.baseURL);
 const isLocal = window.location.hostname === "localhost";
 
 const API = axios.create({
-  baseURL: isLocal 
-    ? 'http://localhost:5000/api' 
-    : 'https://ward-backend-kcW8.onrender.com/api' // <--- यहाँ पक्का करें कि आपका Render बैकएंड लिंक सही है
+  baseURL: isLocal
+    ? "http://localhost:5000/api"
+    : "https://ward-backend-kcw8.onrender.com/api",
 });
+
+console.log("APP START");
+console.log("BASE URL =", API.defaults.baseURL);
 
 export default API;
